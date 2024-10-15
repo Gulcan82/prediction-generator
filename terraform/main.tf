eb7f30b65a4bbb9438098725c1afcd0ae68c2e26
Gülcan Kamat <gulcankamat@gmail.com>
2024-10-15 16:50:15 +0200

    Adding Terraform configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,25 +3,25 @@ provider "aws" {
 }
 
 module "prediction_function_execution_role" {
-    source = "./modules/iam"
-    role_name = "prediction_generator_function_execution_role"
+    source     = "./modules/iam"
+    role_name  = "prediction_generator_function_execution_role"
     policy_name = "AWSLambdaBasicExecutionRole"
 }
 
-module "predition_generator_lambda" {
-    source = "./modules/lambda"
-    function_name = "prediction_generator_function"
-    runtime = "nodejs20.x"
-    handler = "index.handler"
-    lambda_role_arn = module.prediction_function_execution_role.lambda_execution_role_arn
-    lambda_zip_path = "${path.module}/prediction-generator-lambda.zip"
+module "prediction_generator_lambda" {
+    source               = "./modules/lambda"
+    function_name        = "prediction_generator_function"
+    runtime              = "nodejs20.x"
+    handler              = "index.handler"
+    lambda_role_arn      = module.prediction_function_execution_role.lambda_execution_role_arn
+    lambda_zip_path      = "${path.module}/prediction-generator-lambda.zip"
     lambda_environment_vars = {
-      API_TOKEN = "test_value"
+        API_TOKEN = "test_value"
     }  
 }
 
 module "prediction_dynamodb_table" {
-    source = "./modules/dynamodb"
+    source   = "./modules/dynamodb"
     table_name = "predictions"
-    hash_key = "predictionId"
+    hash_key  = "predictionId"
 }
--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,27 +1,25 @@
 resource "aws_iam_role" "lambda_execution_role" {
-    name = var.role_name
+  name = var.role_name
 
-    assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "sts:AssumeRole"
-            ],
-            "Principal": {
-                "Service": [
-                    "lambda.amazonaws.com"
-                ]
-            }
+      {
+        "Effect": "Allow",
+        "Action": [
+          "sts:AssumeRole"
+        ],
+        "Principal": {
+          "Service": [
+            "lambda.amazonaws.com"
+          ]
         }
+      }
     ]
-})
-  
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_execution_role_policy_attachment" {
-    role = aws_iam_role.lambda_execution_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/${var.policy_name}"
-  
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
